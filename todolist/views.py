77a776bf790d6350192b52db6e61b550e24eee9d
Muhamad Sirojudin <rojsiroj@Muhamads-Mac-mini.local>
2020-11-23 18:13:52 +0700

    add form

--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Todolist
+
+from .forms import TodoListForm
+
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 
 def index(request):
-	return render(request,'todolist/index.html')
+	todo_items = Todolist.objects.order_by('id')
+	form = TodoListForm()
+	context = {'todo_items': todo_items, 'form': form}
+	return render(request,'todolist/index.html', context)
+
+@require_POST
+def addTodoItem(request):
+	form = TodoListForm(request.POST)
+
+	if form.is_valid():
+		new_todo = Todolist(text=request.POST['text'])
+		new_todo.save()
+
+	return redirect('index')
